0b9c1b2dba1b6b94ce94df4d4a406028476b299e
PHONG <PHONG@PHONG-PC>
2017-03-14 00:56:06 +0700

    test

---
+++ src/main/java/test/custom/annotation/Test.java
@@ -0,0 +1,12 @@
+package test.custom.annotation;
+
+import java.lang.reflect.Field;
+
+public class Test {
+	public static void main(String[] args) throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException {
+		String a = null;
+		System.out.println(a.toString());
+	}
+	
+	
+}
